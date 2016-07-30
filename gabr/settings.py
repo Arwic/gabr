a02288926cb23dfb4fd9e9b362c18cda22fcc2ea
Tim <timothyjings@gmail.com>
2016-07-31 01:40:18 +0800

    Adds allowed hosts

--- gabr/settings.py
+++ gabr/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.arwic.me']
 
 
 # Application definition
