aafe78747b05210a10277ea0f76b6d9c143b0aea
Tim <timothyjings@gmail.com>
2016-07-30 20:04:22 +0800

    Adds nginx to setup script deps

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install python3 python3-dev
+sudo apt-get install python3 python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx
 sudo pip3 install virtualenv
 virtualenv gabrenv
 . gabrenv/bin/activate
-pip3 install django python-dateutil pillow
+pip3 install django python-dateutil pillow gunicorn
